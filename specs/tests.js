ed193cbf53e859d500d577368e50a0b2e908d478
Giorgio Aquino <gigetto@gmail.com>
2016-05-10 08:09:58 +0200

    More tests

--- specs/tests.js
+++ specs/tests.js
@@ -85,3 +85,46 @@ test('partial_c is imported by a, b and c', function (t) {
       }));
 
 });
+
+test('partial_b is not imported by a', function (t) {
+
+  let sass_dir = path.join(__dirname, 'scss/');
+  let piped_files = [];
+
+  let files = [];
+
+  gulp.src(path.join(sass_dir, '_partial_b.scss'))
+      .pipe(partials(sass_dir))
+      // .pipe(logpipe())
+      // .pipe(through.obj(function (file, enc, cb) {
+      //
+      //   let r = gen.next(file.path);
+      //
+      //   console.log(enc);
+      //
+      //   if (r.done) {
+      //     t.end();
+      //   }
+      //   cb(null, file);
+      //
+      // }))
+      .on('data', function (data) {
+        files.push(data.path)
+      })
+      .on('end', function () {
+
+        t.equal(files.length, 2, "Only two files");
+
+        t.equal(files.indexOf('a.scsss'), -1, "a file not present");
+        t.equal(files.indexOf('b.scsss'), -1, "b file present");
+        t.equal(files.indexOf('c.scsss'), -1, "c file present");
+        // files.forEach(function (file) {
+        //   console.log()
+        //
+        // });
+
+        t.end();
+
+      });
+
+});
